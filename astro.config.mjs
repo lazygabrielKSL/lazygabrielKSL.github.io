1e50a6c01f8fbd77f4056a693cadcd96d1a3289a
lazygabrielKSL <scoremanksl@gmail.com>
2025-02-12 12:49:18 +0800

    Integrate Startlight (for its components)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://lazygabrielksl.github.io/',
-	integrations: [mdx(), sitemap()],
+    site: 'https://lazygabrielksl.github.io/',
+    integrations: [
+        sitemap(), 
+        starlight({
+            title: '軟橘資訊站（暫）',
+        }),
+        mdx()
+    ],
 });
