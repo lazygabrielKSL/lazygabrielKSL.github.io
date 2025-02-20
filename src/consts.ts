d435b0f0c10c78da86dbec06a654f4b2a549211d
Gabriel Kao <scoremanksl@gmail.com>
2025-02-20 18:00:32 +0800

    Update site title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '軟橘資訊網';
+export const SITE_DESCRIPTION = '從日文教學到 Linux 心法，什麼都有的無廣告教學網站';
